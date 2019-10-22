5ffbc5f9f537b190f5d67bc1a34a8ce4304a09c4
rgasper <raymond.j.gasper@gmail.com>
2019-10-22 08:52:16 -0400

    draw some boxes while generating the data

--- synthetic_data_generation/traindatagen.py
+++ synthetic_data_generation/traindatagen.py
@@ -5,7 +5,8 @@ import sys
 import logging
 import argparse
 import traceback
-from random import randint
+from random import choice, sample
+from functools import partial
 from multiprocessing import Pool
 from PIL import Image, ImageDraw, ImageFont
 import csv
@@ -98,14 +99,28 @@ def generate_responses(word, current_x, current_y, font,
 
     return responses
     
+def draw_letter_boxes(word, artist, current_x, current_y, text_w, text_h):
+    ''' draws bounding boxes for letters in word using artist
+    '''
+    num_letters = len(word)
+    letter_w_naieve = text_w/num_letters
+    letter_h_naieve = text_h
+    letter_x, letter_y = current_x, current_y
+    for letter in word:
+        artist.rectangle( [ (letter_x, letter_y) , (letter_x+letter_w_naieve, letter_y+letter_h_naieve) ]
+                    , outline=0, width=0)
+        letter_x += letter_w_naieve
+
 def txt_to_cursive_img(doc, out_path):
     ''' turns a document text into cursive images using the dictionary
         :params:
             doc- list of lines of text
+            out- path to a file to put the image
         :return:
             a PIL Image object, response data, and the font (for debugging, some fonts have issues right now)
     '''
     try:
+        logger.debug(f'generating cursive text image to {out_path}')
         # line_buffer is the pixel spacing between lines
         line_buffer = 15
 
@@ -117,7 +132,7 @@ def txt_to_cursive_img(doc, out_path):
 
         # Get a random font
         fonts = os.listdir("./fonts")
-        font = ImageFont.truetype(f"./fonts/{fonts[randint(0, len(fonts) - 1)]}", 120)
+        font = ImageFont.truetype(f"./fonts/{choice(fonts)}", 120)
 
         # Get max line width and document height
         width, height = get_doc_dimensions(doc, font, side_margin, header_margin, line_buffer)
@@ -132,13 +147,15 @@ def txt_to_cursive_img(doc, out_path):
         responses = []
 
         for line in doc:
-            logger.debug('converting line')
+            # logger.debug('converting line')
             max_height = 0
             for word in line.split():
                 text_w, text_h = draw.textsize(word, font=font)
                 if text_h > max_height:
                     max_height = text_h
                 draw.text((current_x, current_y), word, font=font, fill=0)
+                if args.viz:
+                    draw_letter_boxes(word, artist, current_x, current_y, text_w, text_h)
                 responses.extend(generate_responses(word, current_x, current_y, font, 
                             text_w, out_path))
                 current_x = current_x + text_w + space
@@ -150,7 +167,7 @@ def txt_to_cursive_img(doc, out_path):
         return img, responses, font_out
 
     except:
-        logger.exception()
+        logger.exception(f'failed to generate a cursive image for file {out_path}')
         raise
     
 # NOTE(rgasper) the targetFile API is kinda dumb I'm sure there's a better way to do this 
@@ -185,7 +202,14 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--inputFile", help="input file path for single file", type=str)
     parser.add_argument("-o", "--outputFile", default="test.png", help="output file path", type=str)
     parser.add_argument("-d", "--dir", help="directory of source text files, will run on all .txt files inside the directory")
+    parser.add_argument("-v", "--viz", help="visualize the bounding boxes in the resulting image pages", action='store_true')
+    parser.add_argument("-c", "--count", help="only produce this many output files", type=int)
+    parser.add_argument("-f", "--force", help='overwrite any output directories without user prompt', action='store_true')
+    global args
     args = parser.parse_args()
+
+    if not(args.dir) and not (args.inputFile):
+        raise NotImplementedError('must provide some kind of input to work with')
 
     # do stuff
     if args.inputFile:
@@ -194,21 +218,30 @@ if __name__ == '__main__':
     
     if args.dir:
         outDir = "images_output"
-        logger.info(f'processing all .txt files in {args.dir}')
+        logger.info(f'processing all .txt files in {args.dir}, placing results in {outDir}')
         try:
             os.mkdir(outDir)
         except FileExistsError:
-            response = input(f'Delete all files in directory {outDir} [y/n]?       ')
-            if response.lower().strip()[0] == 'y':
+            response = False
+            if not args.force:
+                response = input(f'Delete all files in directory {outDir} [y/n]?       ')
+                response = response.lower().strip()[0] == 'y'
+            if response or args.force:
                 logger.info(f'cleared all files in {outDir}')
                 for f in os.listdir(outDir):
-                    os.remove(f)
-        infiles =  (f"{args.dir}/{f}" for f in os.listdir(args.dir))
+                    os.remove(f"{outDir}/{f}")
+
+        dirlist = os.listdir(args.dir)
+        if args.count:
+            dirlist = sample(dirlist, args.count)
+        
+        infiles =  (f"{args.dir}/{f}" for f in dirlist)
+
         def to_png(filename):
             splits = filename.split('.')
             return f"{splits[0]}.png"
-        outfiles =  (f"{outDir}/{to_png(f)}" for f in os.listdir(args.dir))
-
+        outfiles =  (f"{outDir}/{to_png(f)}" for f in dirlist)        
+        
         logger.info=(f'starting multiprocess pool iterating over files in {args.dir}')
         pool = Pool(4)
         results = pool.starmap(generate_file, zip(infiles, outfiles))
